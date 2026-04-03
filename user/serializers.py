8a9337f2e9f1770177386796e36d42a4375471b8
Bohdan Borsuk <barsuchellos@gmail.com>
2026-04-03 03:59:39 +0100

    S4

--- user/serializers.py
+++ user/serializers.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth import (get_user_model, authenticate)
 from rest_framework import serializers
 from django.utils.translation import gettext_lazy as _
 
@@ -65,4 +65,5 @@ class AuthTokenSerializer(serializers.Serializer):
 
         attrs['user'] = user
         return attrs
+
 
